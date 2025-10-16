c577d06fb43c37f180005bb63f71a9d452ad3768
AdhithyaSaminathan07 <tsadhithya07@gmail.com>
2025-10-16 16:56:02 +0530

    Update on  Login page 3

--- src/app/(lite)/dashboard/page.tsx
+++ src/app/(lite)/dashboard/page.tsx
@@ -1,3 +1,10 @@
+// import React from 'react';
+// import Dashboard from '@/components/Dashboard';
+
+// export default function DashboardPage() {
+//   return <Dashboard />;
+// }
+
 import React from 'react';
 import Dashboard from '@/components/Dashboard';
 
