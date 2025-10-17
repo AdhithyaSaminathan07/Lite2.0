5b2ac74b0a402b6cc68532dc619af489c4bca799
AdhithyaSaminathan07 <tsadhithya07@gmail.com>
2025-10-17 16:21:20 +0530

    Update.... 3

--- src/app/(lite)/dashboard/page.tsx
+++ src/app/(lite)/dashboard/page.tsx
@@ -11,7 +11,7 @@
 // src/app/(lite)/dashboard/page.tsx
 
 import React from 'react';
-import Dashboard from '@/components/Dashboard';
+import Dashboard from '@/src/components/Dashboard';
 
 export default function DashboardPage() {
   return <Dashboard />;
