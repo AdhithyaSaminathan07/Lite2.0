ba7748686f1b9ef9b89329e9b02b8a3705a96c64
AdhithyaSaminathan07 <tsadhithya07@gmail.com>
2025-10-27 16:01:29 +0530

    Billing...7

--- src/models/Sales.ts
+++ src/models/Sales.ts
@@ -1,38 +1,4 @@
-// // src/models/Sales.ts
 
-// import mongoose, { Schema, models, Document } from "mongoose";
-
-// export interface ISale extends Document {
-//   tenantId: string; // <-- ADD THIS LINE
-//   amount: number;
-//   paymentMethod: 'cash' | 'qr';
-//   createdAt: Date;
-// }
-
-// const SaleSchema = new Schema<ISale>({
-//   tenantId: { // <-- ADD THIS OBJECT
-//     type: String,
-//     required: true,
-//     index: true,
-//   },
-//   amount: {
-//     type: Number,
-//     required: true,
-//   },
-//   paymentMethod: {
-//     type: String,
-//     enum: ["cash", "qr"],
-//     required: true,
-//   },
-//   createdAt: {
-//     type: Date,
-//     default: Date.now,
-//   },
-// });
-
-// const Sale = models.Sale || mongoose.model<ISale>("Sale", SaleSchema);
-
-// export default Sale;
 
 // src/models/Sales.ts
 
